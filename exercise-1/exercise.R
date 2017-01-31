1d54b64175cdd58d183978a4a77788c2866485bc
Daniel Wang <s92025592025@gmail.com>
2017-01-31 15:18:53 -0800

    Participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,13 +1,14 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+library('httr')
+library('jsonlite')
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
-
+base <- 'http://data.unhcr.org/api'
 # For what years does the API have statistical data?
-
+print(fromJSON(content(GET(paste(base, '/stats/time_series_years.json', sep = '')), 'text')))
 
 # What is the "country code" for the "Syrian Arab Republic"?
 
